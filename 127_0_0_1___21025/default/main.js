bbb0fc66950ac092aa44bffc92eb7946020cb7d7
Kyle <klwalters950@gmail.com>
2023-09-08 23:16:48 -0600

    Implementing manageSpawns.js

--- 127_0_0_1___21025/default/main.js
+++ 127_0_0_1___21025/default/main.js
@@ -1,1 +1,37 @@
-test
+const manageSpawns = require('manageSpawns')
+
+
+module.exports.loop = function () {
+
+    const myRooms = getMyRooms()
+
+    for(const roomName of myRooms){
+
+        const room = Game.rooms[roomName];
+
+        manageSpawns(room);
+
+    }
+
+}
+
+/**
+ * Returns an array of names for rooms owned by me.
+ * @returns {string[]}  
+ */
+function getMyRooms() {
+
+    let myRooms = [];
+
+    for (const roomName of Object.keys(Game.rooms)) {
+        if (Game.rooms[roomName].controller.my) {
+
+            myRooms.push(roomName);
+
+        };
+    };
+
+    return myRooms;
+}
+
+
