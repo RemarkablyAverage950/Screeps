013d37b23e93ef6070908bc44b080d9d8d085c3d
Kyle <klwalters950@gmail.com>
2023-10-30 23:01:48 -0600

    remoteMiners now fill their containers with dropped resources if there is room. Dismantlers no longer spawn if storage < 100,000 energy

--- screeps.com/default/expansionManager.js
+++ screeps.com/default/expansionManager.js
@@ -984,7 +984,7 @@ function executeMissions(myRooms) {
                     }
 
                 } else if (mission.type === 'DISMANTLE') {
-                   
+
                     if (room) {
                         let structures = room.find(FIND_STRUCTURES).filter(s => s.structureType !== STRUCTURE_CONTROLLER)
                         if (structures.length === 0 || (room.controller && room.controller.my)) {
@@ -993,7 +993,7 @@ function executeMissions(myRooms) {
                             continue;
                         }
                     }
-                    if(homeRoom.storage[RESOURCE_ENERGY] < 100000){
+                    if (homeRoom.storage.store[RESOURCE_ENERGY] < 100000) {
                         continue;
                     }
                     if (room
